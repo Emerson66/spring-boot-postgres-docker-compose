5cca0971577ee892d07fa853e9a0189ea0fc7980
Emerson66 <emersonreisdacosta@gmail.com>
2022-06-02 19:28:54 -0400

    first commit

---
+++ src/main/java/br/com/buritiscript/dockerspringbootpostgres/DockerSpringBootPostgresApplication.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.dockerspringbootpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/buritiscript/dockerspringbootpostgres/DockerSpringBootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.buritiscript.dockerspringbootpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
